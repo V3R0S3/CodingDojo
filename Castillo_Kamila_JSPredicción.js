a6d206d708eb62b07ab936f4b13be726ad76e0a9
V3R0S3 <kamila.castillo.cabrera@alumnos.sip.cl>
2022-04-07 11:56:42 -0400

    up1

---
+++ Castillo_Kamila_JSPredicción.js
@@ -0,0 +1,28 @@
+Predicción1:
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+myBirthYearFunc()
+    // cuando se llama a la función el consol.log me indica el año en que nací en 1980//
+  
+Predicción2:
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+myBirthYearFunc(2005)
+//cuando se llama a la función el consol.log me va a indicar que nací en 2005//
+
+Predicción3:
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+add(10,20)
+¡Sumando números!
+num1 is: 10
+num2 is: 20
+ 30
+//el consol.log va a indicar el resultado de la suma que es 30//
