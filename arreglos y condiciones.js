e75b9b0293b26dbb2808b769f9f9be039d680ba2
V3R0S3 <kamila.castillo.cabrera@alumnos.sip.cl>
2022-04-12 10:50:22 -0400

    arreglos y condiciones

---
+++ arreglos y condiciones.js
@@ -0,0 +1,33 @@
+
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+/*
+    variables           Datos
+     auntContactInfo =  ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+*/
+
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+
+/*
+    variables       Datos
+    produce   =     ["manzanas", "naranjas"];
+    frozen    =     ["brócoli", "helado"];     
+*/
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+/*
+    variables       Datos
+    movieLibrary  = ["Bambi", "E.T.", "Toy Story"];
+*/
